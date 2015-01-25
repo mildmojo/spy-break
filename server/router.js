3f6a2ad4a638ed5214277d0639ddae7246a405e3
mildmojo <theaterfire@yahoo.com>
2015-01-25 04:15:07 -0500

    Fixes server bugs. Server tracks states correctly!
    All server transitions stubbed with timeouts. Connecting from a
    client and sending a room join message starts things correctly and
    all transitions are... transitioning properly. Now to implement
    game logic server-side and get a UI representation client-side.
    
    Not enough time.

--- server/router.js
+++ server/router.js
@@ -14,9 +14,9 @@ function Router(server) {
   this._clients = [];
   this._rooms = {};
 
-  server.on('connection', this.addClient);
-
   _.bindAll(this);
+
+  server.on('connection', this.addClient);
 }
 
 var $class = Router.prototype;
@@ -29,36 +29,53 @@ $class.addClient = function(socket) {
 };
 
 $class.removeClient = function(client) {
-  client.rooms.forEach(function(room) {
-    room.removeClient(client);
-  });
+  if (client.room) {
+    this.messageRouter(client, '/rooms/' + client.room.id + '|leave');
+  }
   this._clients = _(this._clients).without(client);
-
 };
 
 $class.messageRouter = function(client, data) {
+console.log('GOT MESSSAGE: %s', data);
   // message = '/rooms/4AE6|command|body
   var parts = data.split('|');
   var paths = parts[0].split('/').slice(1); // trim empty string from root slash
   var command = parts[1];
   var body = parts[2];
-
   var commands = {
-    join: function(roomName) {
-      var room = this._rooms[roomName];
-      if (!room) {
-        room = new Room(roomName);
-        this._rooms[roomName] = room;
-        this.emit('room_added', room, client);
-      }
+    create: function(roomName) {
+      var room = new Room(roomName);
+      this._rooms[room.id] = room;
       room.addClient(client);
       client.joinRoom(room);
+      this.emit('room_added', room, client);
+    },
+    join: function(roomName) {
+      var room = this._rooms[roomName];
+      if (!roomName || !room) return commands.create.apply(this, arguments);
+      room.addClient(client);
+      client.joinRoom(room);
+    },
+    rejoin: function(roomName, reconnectToken) {
+      var room = this._rooms[roomName];
+      var oldClient = _(this._clients).find(function(client) {
+        return client.reconnectToken === reconnectToken;
+      });
+      if (oldClient) {
+        client.fromJSON(oldClient.toJSON());
+        room.spliceClient(client);
+        this.emit('resync', room, client);
+      } else {
+        console.log('Bad reconnect');
+        // redirect to front page?
+      }
     },
     leave: function(roomName) {
       var room = this._rooms[roomName];
       if (!room) return;
       room.removeClient(client);
-      if (!room.hasClients) {
+      client.leaveRoom();
+      if (!room.hasClients()) {
         this._rooms = _(this._rooms).omit(roomName);
         this.emit('room_removed', room);
       }
@@ -75,64 +92,97 @@ $class.messageRouter = function(client, data) {
 function Client(socket) {
   if (!(this instanceof Client)) return new Client(socket);
   this.socket = socket;
-  this.rooms = [];
+  this.room = null;
   this.id = this._genID(5);
-
-  this.joinRoom = function(room) {
-    this.rooms.push(room);
-  };
-
-  this.emit = function(message) {
-    this.socket.send(message);
-  };
-
-  this.toJSON = function() {
-    return JSON.stringify({
-      id: this.id
-    });
-  };
-
-  this._genID = function(length) {
-    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var result = '';
-    for (var i = length; i > 0; --i) {
-      result += chars[Math.round(Math.random() * (chars.length - 1))];
-    }
-    return result;
-  };
+  this.reconnectToken = this._genID(5);
 }
+
+var $classClient = Client.prototype;
+
+$classClient.joinRoom = function(room) {
+  this.room = room;
+};
+
+$classClient.leaveRoom = function() {
+  this.room = null;
+};
+
+$classClient.emit = function(message) {
+  this.socket.send(message);
+};
+
+$classClient.toJSON = function() {
+  return JSON.stringify({
+    id: this.id,
+    reconnectToken: this.reconnectToken
+  });
+};
+
+$classClient.fromJSON = function(json) {
+  var attrs = JSON.parse(json);
+  this.id = this.id || attrs.id;
+  this.reconnectToken = this.reconnectToken || attrs.reconnectToken;
+};
+
+$classClient._genID = function(length) {
+  var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var result = '';
+  for (var i = length; i > 0; --i) {
+    result += chars[Math.round(Math.random() * (chars.length - 1))];
+  }
+  return result;
+};
+
 
 function Room(roomName) {
-  this.id = this._genID(5);
+  this.id = roomName || this._genID(5);
   this.clients = [];
-
-  this.addClient = function(client) {
-    this.broadcast('client', 'join', client.toJSON());
-    this.clients.push(client);
-  };
-
-  this.removeClient = function(client) {
-    this.clients = _(this.clients).without(client);
-    this.broadcast('client', 'leave', client.toJSON());
-  };
-
-  this.broadcast = function(message) {
-    var msg = arguments.length > 1 ? this.toMessage.apply(this, arguments) : message;
-    this.clients.forEach(function(client) {
-      client.emit(message);
-    });
-  };
-
-  this._toMessage = function() {
-    return Array.prototype.slice.call(arguments).join(':');
-  };
-
-  this._genID = function(length) {
-    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var result = '';
-    for (var i = length; i > 0; --i) {
-      result += chars[Math.round(Math.random() * (chars.length - 1))];
-    }
-    return result;
-  };
 }
+
+var $classRoom = Room.prototype;
+
+$classRoom.addClient = function(client) {
+  this.broadcast('client', 'join', client.toJSON());
+  this.clients.push(client);
+};
+
+$classRoom.removeClient = function(client) {
+  this.clients = _(this.clients).without(client);
+  this.broadcast('client', 'leave', client.toJSON());
+};
+
+// Silently replace outdated client object with new client & socket.
+$classRoom.spliceClient = function(newClient) {
+  this.clients = _(this.clients).without(function(client) {
+    return client.id === newClient;
+  });
+  this.clients.push(newClient);
+};
+
+$classRoom.hasClients = function() {
+  return !!this.clients.length;
+};
+
+// Send network message to all clients. Argument can be a string or individual
+// args for each field (heading, command, body).
+//   this.broadcast('reset');
+//   this.broadcast('client', 'leave', '{id: "4853e"}');
+$classRoom.broadcast = function(message) {
+  var msg = arguments.length > 1 ? this._toMessage.apply(this, arguments) : message;
+  this.clients.forEach(function(client) {
+    client.emit(message);
+  });
+};
+
+$classRoom._toMessage = function() {
+  return Array.prototype.slice.call(arguments).join(':');
+};
+
+$classRoom._genID = function(length) {
+  var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var result = '';
+  for (var i = length; i > 0; --i) {
+    result += chars[Math.round(Math.random() * (chars.length - 1))];
+  }
+  return result;
+};
--- server/spy_break.js
+++ server/spy_break.js
@@ -10,73 +10,146 @@ function SpyBreak(router) {
   this._router = router;
   this._games = {};
 
+  _.bindAll(this);
+
   router.on('room_added', this.newGame);
   router.on('room_removed', this.stopGame);
-
-  _.bindAll(this);
+  router.on('resync', this.resyncRoom);
 }
 
 var $class = SpyBreak.prototype;
 
 $class.newGame = function(room, client) {
-  this._games[room] = new GameInstance(room, client);
+  var game = new GameInstance(room, client);
+  this._games[room.id] = game;
+  game.start();
 };
 
 $class.stopGame = function(room) {
   var game = this._games[room.id];
   if (!game) return;
-  game.stop();
   this._games = _(this._games).omit(room.id);
+  game.stop();
+};
+
+$class.resyncRoom = function(room, client) {
+  var game = this._games[room.id];
+  if (!game) return;
+  game.sync();
 };
 
 
 function GameInstance(room, clientOwner) {
   this.state = 'lobby';
   this.stateMachine = new EventEmitter();
+  this._intervals = [];
+  this._timeouts = [];
 
   var transitions = {
     '* => lobby': function() {
+      this._clearTimers();
+
       // wait for players to click in
-      this.stateMachine.emit('transition', 'start');
+
+      // TEMP: lose in 10 seconds
+      this._timeouts.push(setTimeout(function() {
+        this.stateMachine.emit('transition', 'lose');
+      }.bind(this), 10000));
+
+      this._timeouts.push(setTimeout(function() { //testing
+        this.stateMachine.emit('transition', 'start');
+      }.bind(this), 1000)); //testing
     },
-    'lobby => start': function() {
+    '* => start': function() {
       // pick challenge
       // start timer
       // send challenge data
       // transition to challenge
-      this.stateMachine.emit('transition', 'challenge');
+      this._timeouts.push(setTimeout(function() { //testing
+        this.stateMachine.emit('transition', 'challenge');
+      }.bind(this), 1000)); //testing
     },
     'start => challenge': function() {
       // handle challenge comms?
       // when challenge requirements met, transition to next_challenge
-      this.stateMachine.emit('transition', 'next_challenge');
+      this._timeouts.push(setTimeout(function() { //testing
+        this.stateMachine.emit('transition', 'next_challenge');
+      }.bind(this), 1000)); //testing
     },
     'challenge => next_challenge': function() {
       // if all challenges complete, transition to win state
       // else transition to start
-      this.stateMachine.emit('transition', 'start');
+      this._timeouts.push(setTimeout(function() { //testing
+        var choice = Math.random() * 2;
+        if (choice > 1) {
+          this.stateMachine.emit('transition', 'start');
+        } else {
+          this.stateMachine.emit('transition', 'win');
+        }
+      }.bind(this), 1000)); //testing
     },
     'next_challenge => win': function() {
+      this._clearTimers();
       // wait for everyone to click in to start again
-      this.stateMachine.emit('transition', 'lobby');
+      this._timeouts.push(setTimeout(function() { //testing
+        this.stateMachine.emit('transition', 'lobby');
+      }.bind(this), 1000)); //testing
     },
     '* => lose': function() {
+      this._clearTimers();
       // show loss
       // wait for everyone to click in
-      this.stateMachine.emit('transition', 'lobby');
+      this._timeouts.push(setTimeout(function() { //testing
+        this.stateMachine.emit('transition', 'lobby');
+      }.bind(this), 1000)); //testing
+    },
+    '* => dying': function() {
+      this._clearTimers();
+      // game is shutting down
+      console.log('Game shutting down.');
     }
   };
 
   this.stateMachine.on('transition', function(newState) {
     var transition = this.state + ' => ' + newState;
     var wildcardTransition = '* => ' + newState;
+    var isTransitioned = false;
+console.log(transition);
     if (transitions[transition]) {
       this.state = newState;
       transitions[transition].call(this);
+      isTransitioned = true;
     }
     if (transitions[wildcardTransition]) {
       this.state = newState;
       transitions[wildcardTransition].call(this);
+      isTransitioned = true;
+    }
+    if (isTransitioned) {
+      this.sync();
     }
   }.bind(this));
+
+  this.start = function() {
+    this.stateMachine.emit('transition', 'lobby');
+  };
+
+  this.stop = function() {
+    this.stateMachine.emit('transition', 'dying');
+  };
+
+  this.sync = function() {
+    // retransmit complete game state to clients
+    // must be idempotent on clients
+  };
+
+  this._clearTimers = function() {
+    this._intervals.forEach(function(intervalID) {
+      clearInterval(intervalID);
+    });
+
+    this._timeouts.forEach(function(timeoutID) {
+      clearTimeout(timeoutID);
+    });
+  };
 }
